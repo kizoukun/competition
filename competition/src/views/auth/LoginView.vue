dd02aaf8b82ca8261df4f987d90436d46caaabf8
kizoukun <kizoukungaming@gmail.com>
2023-07-09 20:08:25 +0700

    Auth design

--- competition/src/views/auth/LoginView.vue
+++ competition/src/views/auth/LoginView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 import api from '@/stores/api';
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 defineOptions({
   name: 'LoginView',
 })
@@ -8,20 +9,12 @@ const form = ref({
   email: '',
   password: ''
 })
+const loading = ref(false);
 const error = ref(null);
-
-async function testData() {
-  api.get('/test')
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      })
-}
 
 async function login() {
   error.value = null;
+  loading.value = true;
   api.post('/auth/login', form.value)
       .then(response => {
         if(response.data.success) {
@@ -36,22 +29,24 @@ async function login() {
         console.error(error);
         error.value = error.message;
       })
+      .finally(() => {
+        loading.value = false;
+      })
 }
 </script>
 <template>
-    <div>
-        <form @submit.prevent="login" class="space-y-4">
-          <label class="block">
-            Email
-            <input class="border border-black" type="email" v-model="form.email" />
-          </label>
-          <label class="block">
-            Password
-            <input class="border border-black" type="password" v-model="form.password" />
-          </label>
+    <div class="min-h-screen flex flex-col items-center justify-center p-8 space-y-4">
+        <h1 class="text-3xl font-bold">Login Now</h1>
+        <p class="text-gray-500 text-lg font-medium">Please enter the details below to continue</p>
+        <form @submit.prevent="login" class="space-y-4 w-full">
+          <input class="w-full bg-gray-200 p-3 rounded-md text-black font-medium" type="email" v-model="form.email" placeholder="Input your email" required />
+          <input class="w-full bg-gray-200 p-3 rounded-md text-black font-medium" type="password" v-model="form.password" placeholder="******" required />
+          <p class="text-end font-medium">Need an account? <router-link to="/auth/register" class="underline text-blue-400 hover:text-blue-500">Register NOW!</router-link></p>
           <p v-if="error" class="text-red-500 text-lg">{{ error }}</p>
-          <button type="submit" class="bg-blue-500 p-2 px-4 rounded-lg hover:bg-blue-600 text-white">Login</button>
+          <button type="submit" class="w-full bg-blue-500 disabled:bg-blue-400 p-2 px-4 rounded-full hover:bg-blue-600 text-white text-lg font-medium" :disabled="loading">
+            <FontAwesomeIcon icon="fa-solid fa-circle-notch mr-2" v-if="loading" spin />
+            LOGIN
+          </button>
         </form>
-        <button type="button" class="bg-green-500 p-2 px-4 rounded-lg text-white" @click="testData">testData</button>
     </div>
 </template>
--- competition/src/views/auth/RegisterView.vue
+++ competition/src/views/auth/RegisterView.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import router from '@/router';
 import api from '@/stores/api';
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 defineOptions({
   name: 'RegisterView',
 })
@@ -15,9 +16,11 @@ const form = ref({
   address: '',
 })
 const error = ref(null);
+const loading = ref(false);
 
 async function register() {
   error.value = null;
+  loading.value = true;
   api.post("/auth/register", form.value)
       .then(response => {
         console.log(response.data);
@@ -31,43 +34,31 @@ async function register() {
         console.error(error);
         error.value = error.message;
       })
+      .finally(() => {
+        loading.value = false;
+      })
 }
 
 </script>
 
 <template>
-  <div>
-    <form @submit.prevent="register" class="space-y-4">
-      <label class="block">
-        Email
-        <input class="border border-black" type="email" v-model="form.email" />
-      </label>
-      <label class="block">
-        Password
-        <input class="border border-black" type="password" v-model="form.password" />
-      </label>
-      <label class="block">
-        Password Confirmation
-        <input class="border border-black" type="password" v-model="form.passwordConfirmation" />
-      </label>
-      <label class="block">
-        First Name
-        <input class="border border-black" type="text" v-model="form.firstName" />
-      </label>
-      <label class="block">
-        Last Name
-        <input class="border border-black" type="text" v-model="form.lastName" />
-      </label>
-      <label class="block">
-        Phone Number
-        <input class="border border-black" type="text" v-model="form.phoneNumber" />
-      </label>
-      <label class="block">
-        Address
-        <input class="border border-black" type="text" v-model="form.address" />
-      </label>
+  <div class="min-h-screen flex flex-col items-center justify-center p-8 space-y-4">
+    <h1 class="text-3xl font-bold">Register Now</h1>
+    <p class="text-gray-500 text-lg font-medium">Please enter the details below to continue</p>
+    <form @submit.prevent="register" class="space-y-4 w-full">
+      <input class="w-full bg-gray-200 p-3 rounded-md text-black font-medium" type="email" v-model="form.email" placeholder="Input your email" required />
+      <input class="w-full bg-gray-200 p-3 rounded-md text-black font-medium" type="password" v-model="form.password" placeholder="******" required />
+      <input class="w-full bg-gray-200 p-3 rounded-md text-black font-medium" type="password" v-model="form.passwordConfirmation" placeholder="Password Confirmation" required />
+      <input class="w-full bg-gray-200 p-3 rounded-md text-black font-medium" type="text" v-model="form.firstName" placeholder="First Name" required />
+      <input class="w-full bg-gray-200 p-3 rounded-md text-black font-medium" type="text" v-model="form.lastName" placeholder="Last Name" required />
+      <input class="w-full bg-gray-200 p-3 rounded-md text-black font-medium" type="text" v-model="form.phoneNumber" placeholder="Phone Number" required />
+      <input class="w-full bg-gray-200 p-3 rounded-md text-black font-medium" type="text" v-model="form.address" placeholder="Address" required />
+      <p class="text-end font-medium">Already have an account? <router-link to="/auth/login" class="underline text-blue-400 hover:text-blue-500">Login NOW!</router-link></p>
       <p v-if="error" class="text-red-500 text-lg">{{ error }}</p>
-      <button type="submit" class="bg-blue-500 p-2 px-4 rounded-lg hover:bg-blue-600 text-white">Register</button>
+      <button type="submit" class="w-full bg-blue-500 disabled:bg-blue-400 p-2 px-4 rounded-full hover:bg-blue-600 text-white text-lg font-medium" :disabled="loading">
+        <FontAwesomeIcon icon="fa-solid fa-circle-notch mr-2" v-if="loading" spin />
+        REGISTER
+      </button>
     </form>
   </div>
 </template>
